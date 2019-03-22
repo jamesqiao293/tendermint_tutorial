fc462376978dfc5a2111d29e0444d47fda66263c
jamesqiao293 <james293@126.com>
2019-03-22 03:25:21 +0000

    first commit

---
+++ c3/counter-checktx.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "fmt"
+  "github.com/tendermint/tendermint/abci/types"
+  "github.com/tendermint/tendermint/abci/server"
+)
+
+type CounterApp struct {
+  types.BaseApplication
+}
+
+func NewCounterApp() *CounterApp {
+  return &CounterApp{}
+}
+
+func (app *CounterApp) CheckTx(tx []byte) (rsp types.ResponseCheckTx) {
+  if tx[0] < 0x04 { 
+    rsp.Log = "tx accepted"
+    return
+  }
+  rsp.Code = 1
+  rsp.Log = "bad tx rejected"
+  return 
+}
+
+func main(){
+  app := NewCounterApp()
+  svr,err := server.NewServer(":26658","socket",app)
+  if err != nil { panic(err) }
+  svr.Start()
+  defer svr.Stop()
+  fmt.Println("abci server started.")
+  select {}
+}
---
+++ c3/counter-initchain.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+  "fmt"
+  "encoding/json"
+  "github.com/tendermint/tendermint/abci/types"
+  "github.com/tendermint/tendermint/abci/server"
+)
+
+type CounterApp struct {
+  types.BaseApplication
+  Value int
+}
+
+func NewCounterApp() *CounterApp {
+  return &CounterApp{}
+}
+
+func (app *CounterApp) InitChain(req types.RequestInitChain) (rsp types.ResponseInitChain){
+  var state map[string]int
+  err := json.Unmarshal(req.AppStateBytes,&state)
+  if err != nil { panic(err) }  
+  app.Value = state["counter"]
+  return
+}
+
+func (app *CounterApp) DeliverTx(tx []byte) (rsp types.ResponseDeliverTx){
+  switch tx[0] {
+    case 0x01 : app.Value += 1
+    case 0x02 : app.Value -= 1
+    case 0x03 : app.Value = 0
+  }
+  rsp.Log = fmt.Sprintf("state updated : %d",app.Value)
+  return
+}
+
+func (app *CounterApp) CheckTx(tx []byte) (rsp types.ResponseCheckTx) {
+  if tx[0] < 0x04 { return }
+  rsp.Code = 1
+  rsp.Log = "bad tx rejected"
+  return 
+}
+
+func main(){
+  app := NewCounterApp()
+  svr,err := server.NewServer(":26658","socket",app)
+  if err != nil { panic(err) }
+  svr.Start()
+  defer svr.Stop()
+  fmt.Println("abci server started.")
+  select {}
+}
